b30d9da09c97d19d69f45b087915dcf97b2cd1f9
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-09-20 15:46:34 +0200

    Add files via upload

--- egyiptom.c
+++ egyiptom.c
@@ -1,19 +1,16 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main()
 {
-int m, n, i=1;
-scanf("%d%d", &m, &n);
-while(m > 0)
-{  
-    while(i*m-n < 0)
-        i++;
-    printf("%d ", i);
-    m = i*m-n;
-    n = i*n;
+    int m, n, i=1;
+    scanf("%d%d", &m, &n);
+    while(m > 0)
+    {
+        while(i*m-n < 0)
+            i = i+1;
+        printf("%d ", i);
+        m = i*m-n;
+        n = i*n;
+    }
+    return 0;
 }
-return 0;
-}
-
-//nevezo n = i*n
-//szamlalo m = i*m-n
