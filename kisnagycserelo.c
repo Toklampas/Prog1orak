2e65a1977405c4a91ae13b05d6cf0d5fdab2e32d
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-10-02 20:55:05 +0200

    Finished the program

--- kisnagycserelo.c
+++ kisnagycserelo.c
@@ -3,18 +3,12 @@
 int main()
 {
     char a;
-    while(scanf("%c", &a) == 1);
+    while(scanf("%c", &a) == 1)
     {
-        if ('a'>=97 && 'a'<=122)
-        {
-            a = 'a'-32;
-            printf("%c", a);
-        }     
-        else if ('a'>=65 && 'a'<=90)
-        {
-            a = 'a'+32;
-            printf("%c", a);
-        } 
+        if (a>=97 && a<=122)
+            printf("%c", a-32);   
+        else if (a>=65 && a<=90)
+            printf("%c", a+32);
         else
             printf("%c", a);
     }
