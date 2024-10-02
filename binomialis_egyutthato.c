2c636707b84e1ed8933073466caf77b09a18ef8a
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-10-02 09:16:50 +0200

    Finished the program

--- binomialis_egyutthato.c
+++ binomialis_egyutthato.c
@@ -2,28 +2,32 @@
 
 int main()
 {
-    unsigned long long n, k, lnko, eredmeny;
+    unsigned long long n, k, lnko, eredmeny=1;
     int num[40]={0}, den[40]={0};
     scanf("%llu%llu", &n, &k);
-    for (int i = 0; i < n-(n-k); i++)
+    if (k > n-k)
+        k = n-k;
+    for (int i = 0; i < k; i++)
     {
         num[i] = n-i;
-        den[i] = (n-(n-k))-i;
+        den[i] = k-i;
     }
     for (int i = 0; i < k; i++) 
     {
         for (int j = 0; j < k; j++) 
         {
-            while(den[j] > 0)
+            int a = num[i];
+            int b = den[j];
+            while(b > 0)
             {
-                int c = den[j];
-                den[j] = num[i]%den[j];
-                num[i] = c;
+                int c = b;
+                b = a%b;
+                a = c;
             }
-            if (lnko > 1)
+            if (a > 1)
             {
-                num[i] /= lnko;
-                den[j] /= lnko;
+                num[i] /= a;
+                den[j] /= a;
             }
         }
     }
@@ -32,3 +36,5 @@ int main()
     printf("%llu", eredmeny);
     return 0;
 }
+
+//32153 32150
