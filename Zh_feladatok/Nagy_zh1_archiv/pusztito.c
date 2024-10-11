d7db50fb2946de1eb3e3bf41f35003256dd661a7
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-10-11 16:32:21 +0200

    Update pusztito.c

--- Zh_feladatok/Nagy_zh1_archiv/pusztito.c
+++ Zh_feladatok/Nagy_zh1_archiv/pusztito.c
@@ -2,18 +2,19 @@
 
 int main()
 {
-    double load = 0.0, in;
+    double load = 0.0, in=0.0;
     while (scanf("%lf", &in) == 1)
     {
         if (load+in > 2.0)
         {
             printf("%.3f ", 2-load);
             load = 0;
+            load += in;
         }
         else
             load += in;
     }
     if (load != 0.0)
-        printf("%.3f", load);
+        printf("%.3f", 2-load);
     return 0;
 }
