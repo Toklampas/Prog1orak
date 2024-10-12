461794391b4304d3304848277704127f266ca079
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-10-12 14:30:35 +0200

    Does not work yet

---
+++ Zh_feladatok/Nagy_zh1_archiv/intervallum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int interval(int start, int length)
+{
+    int n = 0;
+    for (int i = start; i < start + length; i++)
+        if ((i/10)%10 < i%10)
+            n++;
+    return n;
+}
+
+int main()
+{
+    int start, length, min = -1;
+    while (scanf("%d %d", &start, &length) == 2)
+        if (interval(start, length) < min || min == -1)
+            min = interval(start, length);
+    printf("%d", min);
+}
