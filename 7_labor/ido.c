b1ee2f24e7acbbc438a97c25c4345199b9e474e1
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-10-18 15:12:40 +0200

    Update ido.c

--- 7_labor/ido.c
+++ 7_labor/ido.c
@@ -14,45 +14,64 @@ int time2sec(time t)
     return sum;
 }
 
-time sec2time(int sec_since_midnight)
+time sec2time(int s)
 {
-    int hour=0, min=0, sec=0;
     time t;
-    sec = sec_since_midnight;
-    if (sec_since_midnight > 0)
-    {
-        while (sec >= 60)
-        {
-            min++;
-            sec -= 60;
-        }
-        while (min >=60)
-        {
-            hour++;
-            min -= 60;
-        }
-    }
-    else if (sec_since_midnight < 0)
-    {
-        hour = 23; min = 59; sec = 59;
-        while (sec_since_midnight >= -60)
-        {
-            min--;
-            sec -= 60;
-        }
-        while (min > 0)
-        {
-            hour--;
-            min -= 60;
-        }
-    }
-    
-    t.hour = hour;
-    t.min = min;
-    t.sec = sec;
+    s = s%86400;
+    if (s < 0)
+        s += 86400;
+    t.hour = s/3600;
+    s %= 3600;
+    t.min = s/60;
+    s %= 60;
+    t.sec = s;
     return t;
 }
 
+time timeplus(time a, time b)
+{
+    int sum = time2sec(a)+time2sec(b);
+    return sec2time(sum);
+}
+
+int timecmp(time a, time b)
+{
+    if (time2sec(a) < time2sec(b))
+        return -1;
+    else if (time2sec(a) > time2sec(b))
+        return 1;
+    else
+        return 0;
+}
+
+void timereset(time *p)
+{
+    (*p).hour = 0;
+    (*p).min = 0;
+    (*p).sec = 0; 
+}
+
+int main(void)
+{
+    time workout_begins = {23, 15, 0};
+    timereset(&workout_begins);
+    return 0;
+}
+
+/*
+#include <stdio.h>
+int main()
+{
+    time workout_begins = {10, 15, 0};
+    time workout_duration = {1, 40, 0};
+    time workout_ends = timeplus(workout_begins, workout_duration);
+    if (timecmp(workout_ends, workout_begins) < 0)
+        printf("Hamarabb van vege, mint mikor kezdodik\n");
+    return 0;
+}
+*/
+
+/*
 int main(void)
 {
     time t1 = {2, 5, 0};
@@ -61,3 +80,4 @@ int main(void)
     time t3 = sec2time(-600);   // 23:50:00
     return 0;
 }
+*/
