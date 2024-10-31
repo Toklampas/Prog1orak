77a264a54d7c73656d3ec657505bdf35b2ae50b4
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-10-31 21:47:36 +0100

    Update aritmetika_nagy_szamokkal.c

--- IMSc_feladatok/aritmetika_nagy_szamokkal.c
+++ IMSc_feladatok/aritmetika_nagy_szamokkal.c
@@ -6,3 +6,14 @@ typedef struct {
     unsigned length;
     unsigned char* number;
 } big_number_t;
+
+big_number_t create(char* s)
+{
+    int len = strlen(s);
+    big_number_t result;
+    result.length = (len + 1) / 2;
+    result.number = (unsigned char*)calloc(result.length, sizeof(unsigned char));
+
+
+    
+}
