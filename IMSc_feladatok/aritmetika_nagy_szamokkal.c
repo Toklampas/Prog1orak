def73276ada9844b38faff746e0a619a8cd4b026
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-11-01 14:42:55 +0100

    Update aritmetika_nagy_szamokkal.c

--- IMSc_feladatok/aritmetika_nagy_szamokkal.c
+++ IMSc_feladatok/aritmetika_nagy_szamokkal.c
@@ -19,21 +19,41 @@ big_number_t create(char* s)
     return out;
 }
 
-char *tostring(big_number_t input)
+char *tostring(big_number_t a)
 {
     //minden bajt ket karakter, plusz 1 a lezaro 0 miatt
-    char* str = (char*)calloc(input.length * 2 + 1, sizeof(char));
-    for (int i = 0; i < input.length; i++)
+    char* str = (char*)calloc(a.length * 2 + 1, sizeof(char));
+    for (int i = 0; i < a.length; i++)
+    {
         //str-ben kettesevel haladunk, inputon forditott sorrendben megyunk vegig
-        sprintf(&str[i * 2], "%2x", input.number[input.length - 1 - i]);
+        sprintf(&str[i * 2], "%2x", a.number[a.length - 1 - i]);
+        //ha a szam csak egy karaktert foglal, a masik karakter legyen nulla
+        if (str[i*2] == ' ')
+            str[i*2] = '0'; 
+    }
     return str;
 }
 
-void destroy(big_number_t *input)
+void destroy(big_number_t *a)
 {
-    free((*input).number);
-    (*input).length = 0;
-    (*input).number = NULL;
+    free((*a).number);
+    (*a).length = 0;
+    (*a).number = NULL;
+}
+
+big_number_t add (big_number_t a, big_number_t b)
+{
+    big_number_t out;
+    //annyi lesz a kimenet hossza amennyi a hosszabb input, plusz 1 ha tortenik atvitel
+    if (a.length > b.length)
+        out.length = a.length + 1;
+    else
+        out.length = b.length + 1;
+    out.number = (unsigned char*)calloc(out.length, sizeof(unsigned char));
+    for (int i = 0; i < out.length; i++)
+    {
+    }
+    return out;
 }
 
 int main()
