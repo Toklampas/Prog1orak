5d6510cb8f25649e8bef858ee08a66e72e2404ad
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-11-16 22:19:17 +0100

    Update lego.c

--- Nagy_hazi/lego.c
+++ Nagy_hazi/lego.c
@@ -30,16 +30,28 @@ alkatresz_t* doboz_beolvas(char *fajlnev, unsigned *n)
 
     while (fgets(sor, 50, doboz_fajl) != 0)
     {
-        alkatresz_t *uj_alkatreszek = realloc(alkatreszek, (*n + 1) * sizeof(alkatresz_t));
-        if (uj_alkatreszek == NULL)
+        alkatresz_t *uj_alkatresz = malloc(sizeof(alkatresz_t));
+        if (uj_alkatresz == NULL)
         {
-            free(alkatreszek);
+            while (alkatreszek != NULL)
+            {
+                alkatresz_t *temp = alkatreszek;
+                alkatreszek = alkatreszek->next;
+                free(temp);
+            }
             fclose(doboz_fajl);
             return NULL;
         }
-        alkatreszek = uj_alkatreszek;
-        if (sscanf(sor, "%s %d db", alkatreszek[*n].id, &alkatreszek[*n].darab) == 2)
+        if (sscanf(sor, "%s %d db", uj_alkatresz->id, &uj_alkatresz->darab) == 2)
+        {
+            uj_alkatresz->next = alkatreszek;
+            alkatreszek = uj_alkatresz;
             (*n)++;
+        }
+        else
+        {
+            free(uj_alkatresz);
+        }
     }
     fclose(doboz_fajl);
     return alkatreszek;
@@ -64,29 +76,57 @@ keszlet_t* keszlet_beolvas(char *fajlnev, unsigned *n)
         keszlet_t *uj_keszletek = realloc(keszletek, (*n + 1) * sizeof(keszlet_t));
         if (uj_keszletek == NULL)
         {
+            for (int i = 0; i < keszlet_elemszam; i++) {
+                alkatresz_t *current = keszletek[i].alkatreszek;
+                while (current != NULL) {
+                    alkatresz_t *temp = current;
+                    current = current->next;
+                    free(temp);
+                }
+            }
             free(keszletek);
             fclose(keszlet_fajl);
             return NULL;
         }
         keszletek = uj_keszletek;
+        keszlet_t *jelenlegi_keszlet = &keszletek[*n];
+        jelenlegi_keszlet->alkatreszek = NULL;
+        jelenlegi_keszlet->alkatreszfajta_darab = 0;
 
         //nev beolvasasa
-        strncpy(keszletek[*n].nev, sor, 150);
-        keszletek[*n].nev[sizeof(keszletek[*n].nev) - 1] = '\0';
+        strncpy(jelenlegi_keszlet->nev, sor, 150);
+        jelenlegi_keszlet->nev[sizeof(jelenlegi_keszlet->nev) - 1] = '\0';
 
         //epitoelemek beolvasasa
-        keszlet_t *k = &keszletek[*n];
-        if (fgets(sor, 1000, keszlet_fajl) != 0)
+        if (fgets(sor, 1000, keszlet_fajl) != NULL)
         {
-            keszletek[*n].alkatreszfajta_darab = 0;
             char *string_reszlet = strtok(sor, " ");
-            while (string_reszlet != NULL && keszletek[*n].alkatreszfajta_darab < 1500)
+            while (string_reszlet != NULL)
             {
-                strncpy(keszletek[*n].alkatreszek[keszletek[*n].alkatreszfajta_darab].id, string_reszlet, sizeof(keszletek[*n].alkatreszek[keszletek[*n].alkatreszfajta_darab].id));
-                keszletek[*n].alkatreszek[keszletek[*n].alkatreszfajta_darab].id[sizeof(keszletek[*n].alkatreszek[keszletek[*n].alkatreszfajta_darab].id)-1] = '\0';
+                alkatresz_t *uj_alkatresz = malloc(sizeof(alkatresz_t));
+                if (uj_alkatresz == NULL)
+                {
+                    alkatresz_t *jelenlegi_alkatreszek = jelenlegi_keszlet->alkatreszek;
+                    while (jelenlegi_alkatreszek != NULL)
+                    {
+                        alkatresz_t *temp = jelenlegi_alkatreszek;
+                        jelenlegi_alkatreszek = jelenlegi_alkatreszek->next;
+                        free(temp);
+                    }
+                    free(keszletek);
+                    fclose(keszlet_fajl);
+                    return NULL;
+                }
+
+                strncpy(uj_alkatresz->id, string_reszlet, sizeof(uj_alkatresz->id));
+                uj_alkatresz->id[sizeof(uj_alkatresz->id) - 1] = '\0';
                 string_reszlet = strtok(NULL, " ");
-                if (string_reszlet != NULL && sscanf(string_reszlet, "%d", &keszletek[*n].alkatreszek[keszletek[*n].alkatreszfajta_darab].darab) == 1)
-                    keszletek[*n].alkatreszfajta_darab++;
+                if (string_reszlet != NULL && sscanf(string_reszlet, "%d", &uj_alkatresz->darab) == 1)
+                {
+                    uj_alkatresz->next = jelenlegi_keszlet->alkatreszek;
+                    jelenlegi_keszlet->alkatreszek = uj_alkatresz;
+                    jelenlegi_keszlet->alkatreszfajta_darab++;
+                }
                 string_reszlet = strtok(NULL, " ");
                 string_reszlet = strtok(NULL, " ");
             }
@@ -95,7 +135,7 @@ keszlet_t* keszlet_beolvas(char *fajlnev, unsigned *n)
 
         //ar beolvasasa
         if (fgets(sor, 1000, keszlet_fajl) != 0)
-            sscanf(sor, "%d", &keszletek[*n].ar);
+            sscanf(sor, "%d", &jelenlegi_keszlet->ar);
         else break;
         (*n)++;
         fgets(sor, 1000, keszlet_fajl);
@@ -107,13 +147,16 @@ keszlet_t* keszlet_beolvas(char *fajlnev, unsigned *n)
 
 int kirakhato_e(keszlet_t *keszlet, alkatresz_t *doboz, unsigned doboz_n)
 {
-    int eleg_ez_a_darab = 0;
-    for (int i = 0; i < (*keszlet).alkatreszfajta_darab; i++)
+    for (; keszlet->alkatreszek != NULL; keszlet->alkatreszek = keszlet->alkatreszek->next)
     {
+        int eleg_ez_a_darab = 0;
         for (int j = 0; j < doboz_n; j++)
-            if (strcmp((*keszlet).alkatreszek[i].id, doboz[j].id) == 0)
-                if (doboz[j].darab >= (*keszlet).alkatreszek[i].darab)
+            if (strcmp(keszlet->alkatreszek->id, doboz[j].id) == 0)
+                if (doboz[j].darab >= keszlet->alkatreszek->darab)
+                {
                     eleg_ez_a_darab = 1;
+                    break;
+                }
         if (!eleg_ez_a_darab)
             return 0;
     }
@@ -146,13 +189,18 @@ int main()
     keszlet_t *keszletek = keszlet_beolvas("keszletek.txt", &keszlet_elemszam);
     if (keszletek == NULL)
     {
-        free(doboz_alkatreszek);
+        alkatresz_t *temp;
+        while (doboz_alkatreszek != NULL) {
+            temp = doboz_alkatreszek;
+            doboz_alkatreszek = doboz_alkatreszek->next;
+            free(temp);
+        }
         return 1;
     }
 
     keszlet_t *legdragabb_keszlet = legdragabb_kirakhato_keszlet(keszletek, keszlet_elemszam, doboz_alkatreszek, doboz_elemszam);
     if (legdragabb_keszlet != NULL)
-        printf("A legdrágább kirakható készlet: %s", (*legdragabb_keszlet).nev);
+        printf("A legdrágább kirakható készlet: %s", legdragabb_keszlet->nev);
     else
         printf("Egyik készlet sem rakható ki a dobozban lévő alkatrészekkel");
     free(doboz_alkatreszek);
