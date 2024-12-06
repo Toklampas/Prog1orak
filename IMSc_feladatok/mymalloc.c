863751bf1a226a8f202f5059d77f5d03929a907c
Toklampas <161125099+Toklampas@users.noreply.github.com>
2024-12-06 16:01:41 +0100

    Update mymalloc.c

--- IMSc_feladatok/mymalloc.c
+++ IMSc_feladatok/mymalloc.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
 #include <stddef.h>
-#include <string.h>
-
-#define MEMORY_SIZE 500  // Adjusted size for testing
 
 typedef struct Block {
     struct Block* next;
@@ -82,59 +78,4 @@ void own_free(void* ptr)
         block->size += sizeof(Block) + current->size;
         block->next = current->next;
     }
-}
-
-char memory_arena[MEMORY_SIZE];
-
-void reset_memory_arena() {
-    memset(memory_arena, 0, MEMORY_SIZE);
-    free_blocks = NULL;
-}
-
-// Function to initialize free blocks manually
-void initialize_free_blocks(Block* blocks[], size_t sizes[], int count) {
-    reset_memory_arena();
-    size_t offset = 0;
-    for (int i = 0; i < count; i++) {
-        blocks[i] = (Block*)(memory_arena + offset);
-        blocks[i]->size = sizes[i];
-        blocks[i]->next = NULL;
-        offset += sizeof(Block) + sizes[i];
-        if (i > 0) {
-            blocks[i - 1]->next = blocks[i];
-        }
-    }
-    free_blocks = blocks[0];
-}
-
-void print_free_blocks() {
-    Block* current = (Block*)free_blocks;
-    printf("Free blocks:\n");
-    while (current) {
-        printf("  Block at %td, size = %zu bytes\n", (ptrdiff_t)((char*)current - memory_arena), current->size);
-        current = current->next;
-    }
-}
-
-int main(void) {
-    printf("Test Case #6: One free block (100 bytes excluding header), calling own_malloc(100 - HEADER_SIZE)\n");
-    {
-        Block* blocks[1];
-        size_t sizes[1] = {100};
-        initialize_free_blocks(blocks, sizes, 1);
-
-        size_t alloc_size = 100 - sizeof(Block);
-        void* ptr = own_malloc(alloc_size);
-        printf("Allocated %zu bytes at offset %td\n", alloc_size, (ptrdiff_t)((char*)ptr - memory_arena));
-
-        printf("After allocation:\n");
-        print_free_blocks();
-
-        // Expected: free_blocks is NULL
-        if (free_blocks == NULL)
-            printf("free_blocks is NULL as expected.\n");
-        else
-            printf("free_blocks is not NULL, which is incorrect.\n");
-    }
-    return 0;
 }
